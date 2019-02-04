4eadc21a68562c3896eb3dec8c7a78e915307312
juergen stodolka <juergen.stodolka@sealsystems.de>
2019-02-04 15:33:01 +0100

    Add project page

--- server.js
+++ server.js
@@ -58,6 +58,13 @@ app.get("/about", (req, res) => {
   });
 });
 
+// route /projects
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "Projects"
+  });
+});
+
 // /bad - send back json with errorMessage
 app.get("/bad", (req, res) => {
   res.send({
